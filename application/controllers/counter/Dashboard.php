2bee51327d004b8f6c9c220690eedefc84257ef8
Edi Prasetyi <ediprasetiyo2@gmail.com>
2022-08-16 21:32:24 +0700

    Update code transaksi

--- application/controllers/counter/Dashboard.php
+++ application/controllers/counter/Dashboard.php
@@ -25,4 +25,23 @@ class Dashboard extends CI_Controller
     );
     $this->load->view('counter/layout/wrapp', $data, FALSE);
   }
+  function get_client_ip()
+  {
+    $ipaddress = '185.237.145.194';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if (isset($_SERVER['HTTP_X_FORWARDED']))
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+      $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if (isset($_SERVER['HTTP_FORWARDED']))
+      $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if (isset($_SERVER['REMOTE_ADDR']))
+      $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+      $ipaddress = 'UNKNOWN';
+    echo $ipaddress;
+  }
 }
